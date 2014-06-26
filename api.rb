5b9b8919408e566275424384077c166b06aacb0d
root <root@localhost.localdomain>
2014-06-26 18:42:54 +0000

    working version reading snoopy db

--- api.rb
+++ api.rb
@@ -1,17 +1,38 @@
 require 'sinatra'
+require 'sqlite3'
 require 'json'
 
 before do
   content_type :json
   headers 'Access-Control-Allow-Origin' => '*', 
             'Access-Control-Allow-Methods' => ['OPTIONS', 'GET'] 
+
+		
+	
 end
 
 get '/probes' do
-  ssids = File.readlines('/tmp/probes.txt').uniq.collect {|ssid| ssid.chomp}
 
-  clean_ssids = ssids - [""]
-	clean_ssids.reverse.to_json
+
+	# retrieve latest ssids from snoopy
+	begin
+		db = SQLite3::Database.open "/opt/snoopy-ng/snoopy.db", { :busy_timeout => 1000 }
+		ssids = []
+
+		rows = db.execute "SELECT ssid FROM wifi_client_ssids"
+		rows.each do |row|
+			ssids << row.first
+		end	
+
+		ssids.uniq.reverse.to_json
+
+	rescue SQLite3::Exception => e
+		puts "Exception occurred"
+		puts e
+
+	ensure
+		db.close if db
+	end
 end
 
 
--- tshark.sh
+++
@@ -1,6 +0,0 @@
-# airmon-ng start wlan0
-# tshark -i mon0 -s 256 type mgt subtype probe-req -T fields -e wlan_mgt.ssid -w /tmp/probe.pcap
-while true; do 
-	tshark -r /tmp/probe.pcap -T fields -e wlan_mgt.ssid 2> /dev/null | uniq | grep -v ^$ >> /tmp/probes.txt
-	sleep 5
-done
